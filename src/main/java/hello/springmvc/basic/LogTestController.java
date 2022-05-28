38b6f2e19f8b999372725787010b7962ab5927aa
skypotato <hunsooski@naver.com>
2022-05-28 20:23:13 +0900

    2021.05.28 : 로깅 간단히 알아보기 #1

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -13,7 +13,11 @@ public class LogTestController {
     public String logTest(){
         String name = "spring";
 
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
         log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
 
         return "ok";
     }
