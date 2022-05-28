9bf3df8f323a92082cee39802fb1c0bda25b7d48
skypotato <hunsooski@naver.com>
2022-05-29 07:21:17 +0900

    2021.05.29 : 요청 매핑 - API 예시 #1

--- src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -2,18 +2,16 @@ package hello.springmvc.basic.requestmapping;
 
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * 회원 목록 조회: GET   /users
+ * 회원 등록    : POST  /users
+ * 회원 조회    : GET   /users/{userId}
+ * 회원수정     : PATCH /users/{userId}
+ * 회원 삭제    : DELETE /users/{userId}
+ */
 @RestController
 public class MappingClassController {
 
-    /**
-     * 회원 목록 조회: GET   /users
-     * 회원 등록    : POST  /users
-     * 회원 조회    : GET   /users/{userId}
-     * 회원수정     : PATCH /users/{userId}
-     * 회원 삭제    : DELETE /users/{userId}
-     *
-     * @return
-     */
     @GetMapping("/mapping/users")
     public String users() {
         return "get users";
