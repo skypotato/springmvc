2443a7e295910e5f954c6edaa40c33b0fad0d339
skypotato <hunsooski@naver.com>
2022-05-28 20:44:36 +0900

    2021.05.28 : 요청 매핑 #1

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -3,6 +3,7 @@ package hello.springmvc.basic.requestmapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +11,7 @@ public class MappingController {
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
-    @RequestMapping("/hello-basic")
+    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
     public String helloBasic(){
         log.info("helloBasic");
         return "ok";
