5ac21adfef226a8715a65bbd963967193008dd60
skypotato <hunsooski@naver.com>
2022-05-29 15:25:25 +0900

    2021.05.29 : HTTP 요청 파라미터 - @ModelAttribute #1

--- src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -1,6 +1,7 @@
 package hello.springmvc.basic.request;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,8 @@ import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
@@ -23,5 +26,23 @@ public class RequestBodyStringController {
 
         response.getWriter().write("ok");
 
+    }
+
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+
+        responseWriter.write("ok");
+    }
+
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) {
+        String messageBody = httpEntity.getBody();
+
+        log.info("messageBody={}", messageBody);
+
+        return new HttpEntity<>("ok");
     }
 }
